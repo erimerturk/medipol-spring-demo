10bd1d9fccd5d2e7e1ff32c7af1eace3c32d90e9
erimerturk <erimerturk@gmail.com>
2017-05-31 08:39:36 +0300

    ecommerce sample

---
+++ src/main/java/com/medipol/ecommerce/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.medipol.ecommerce.service;
+
+import com.medipol.ecommerce.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<Product>();
+
+    public ProductService() {
+
+        products.add(new Product(1, "nike air", 200));
+        products.add(new Product(2, "addidas", 300));
+    }
+
+    public void add(){
+        //TODO
+    }
+
+    public void delete(Integer id) {
+        //TODO
+    }
+
+    public void update(){
+        //TODO
+    }
+
+    public List<Product> list(){
+        return products;
+    }
+}
